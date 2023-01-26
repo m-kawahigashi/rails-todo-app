1ff6dc6eb06430b542650208cf3dd6597df327f1
m-kawahigashi <masanori0624ds16@yahoo.co.jp>
2023-01-26 23:47:04 +0900

    add: タスク一覧

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,33 @@
+class TasksController < ApplicationController
+    before_action :set_task, only: [:edit, :update, :destroy]
+
+    def index
+        @tasks = Task.select(:task, :user_id).where(user_id: current_user.id)
+        @input_task = Task.new
+        # binding.pry
+
+        @no_tasks_message = "タスクが登録されていません。" if @tasks.empty?
+    end
+
+    def create
+        # binding.pry
+        @task = Task.new(task_params)
+
+        begin
+            @task.save!
+        rescue => e
+            # binding.pry
+            @err_message = "タスクの登録に失敗しました。"
+        end
+        redirect_to root_path
+    end
+
+    private
+    def task_params
+        params.require(:task).permit(:task, :user_id).merge(user_id: current_user.id)
+    end
+
+    def set_task
+        @current_task = Task.find(params[:id])
+    end
+end
